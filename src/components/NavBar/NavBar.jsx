ce5de2a74ef1c471a00a6bde21b65a6c9bdac270
Sayed Haider <sayedhaider2002@gmail.com>
2025-03-23 13:01:54 +0300

    Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,6 +5,7 @@ import { useContext } from "react";
 
 const NavBar = ({ handleSignout }) => {
   const user = useContext(AuthedUserContext);
+
   return (
     <nav className="navbar">
       <div className="logo">
@@ -14,57 +15,36 @@ const NavBar = ({ handleSignout }) => {
       <ul className="nav-links">
         {!user && (
           <>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/signin">Sign In</Link></li>
           </>
         )}
 
         {user && user.role === "admin" && (
           <>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <button onClick={handleSignout}>Sign Out</button>
-            </li>
+            <li><Link to="/">Dashboard</Link></li>
+            <li><button onClick={handleSignout}>Sign Out</button></li>
           </>
         )}
 
         {user && user.role === "dealer" && (
           <>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <button onClick={handleSignout}>Sign Out</button>
-            </li>
+            <li><Link to="/">Dashboard</Link></li>
+            <li><Link to="/dealer/cars/rentals">My Cars & Rentals</Link></li>
+            <li><Link to="/dealer/cars/new">Add Car</Link></li>
+            <li><Link to="/dealer/requests">Rental Requests</Link></li>
+            <li><button onClick={handleSignout}>Sign Out</button></li>
           </>
         )}
 
         {user && user.role === "user" && (
           <>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/cars">Cars</Link>
-            </li>
-            <li>
-              <Link to="/my-rentals">My Rentals</Link>
-            </li>
-            <li>
-              <Link to="/become-dealer">Become a Dealer</Link>
-            </li>
-            <li>
-              <button onClick={handleSignout}>Sign Out</button>
-            </li>
+            <li><Link to="/">Dashboard</Link></li>
+            <li><Link to="/cars">Cars</Link></li>
+            <li><Link to="/my-rentals">My Rentals</Link></li>
+            <li><Link to="/become-dealer">Become a Dealer</Link></li>
+            <li><button onClick={handleSignout}>Sign Out</button></li>
           </>
         )}
       </ul>
